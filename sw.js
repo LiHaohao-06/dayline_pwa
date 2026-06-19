9cfd67ef392472b03b55a99f33d18c4b0329c4dd
LiHaohao-06 <LiHaohao-06@users.noreply.github.com>
2026-06-20 01:36:14 +0800

    Update Dayline PWA focus timer support

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dayline-pwa-v4';
+const CACHE_NAME = 'dayline-pwa-v5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
