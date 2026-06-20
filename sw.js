ec9462a8732aa39dbeccb0bf5ac1ed83101f1b12
LiHaohao-06 <LiHaohao-06@users.noreply.github.com>
2026-06-20 20:47:35 +0800

    Publish AI error handling PWA update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dayline-pwa-v6';
+const CACHE_NAME = 'dayline-pwa-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
